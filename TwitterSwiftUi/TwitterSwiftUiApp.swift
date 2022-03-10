aec996617bdba5d944161f431e2e439c7fdb895e
Raphael Freitas <o.raphaelfreitas@gmail.com>
2022-03-10 19:27:38 -0300

    create login view

--- TwitterSwiftUi/TwitterSwiftUiApp.swift
+++ TwitterSwiftUi/TwitterSwiftUiApp.swift
@@ -12,7 +12,8 @@ struct TwitterSwiftUiApp: App {
     var body: some Scene {
         WindowGroup {
             NavigationView {
-                ContentView()
+//                ContentView()
+                LoginView()
             }
         }
     }
