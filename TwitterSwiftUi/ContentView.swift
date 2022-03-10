9af6d7019b56f0968c048841ece491113a7c2b7d
Raphael Freitas <o.raphaelfreitas@gmail.com>
2022-03-10 10:55:42 -0300

    Initial Commit

---
+++ TwitterSwiftUi/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUi
+//
+//  Created by Raphael Freitas da Silva on 10/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TwitterSwiftUi/TwitterSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSwiftUiApp.swift
+//  TwitterSwiftUi
+//
+//  Created by Raphael Freitas da Silva on 10/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
