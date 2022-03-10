a5aa2ca80013563ecc0a7a3376d3ccae83a95a8d
Raphael Freitas <o.raphaelfreitas@gmail.com>
2022-03-10 19:59:33 -0300

    install firebase and configure

--- TwitterSwiftUi/TwitterSwiftUiApp.swift
+++ TwitterSwiftUi/TwitterSwiftUiApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct TwitterSwiftUiApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             NavigationView {
